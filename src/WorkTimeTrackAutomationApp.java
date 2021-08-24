b63c93f7c06928a444303360367d923fad81e994
chrzanof <58990661+chrzanof@users.noreply.github.com>
2021-08-24 14:29:16 +0200

    Implemented saving start and finish timestamp at user command

--- src/WorkTimeTrackAutomationApp.java
+++ src/WorkTimeTrackAutomationApp.java
@@ -1,5 +1,29 @@
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.time.Period;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class WorkTimeTrackAutomationApp {
     public static void main(String[] args) {
-        System.out.println("hello, world!");
+        WorkingDay workingDay = new WorkingDay();
+        workingDay.setStart();
+        System.out.println("Started working at: " + workingDay.getStart());
+        try(Scanner scanner = new Scanner(System.in)) {
+            String userStr = "";
+            while(!userStr.equals("finish")) {
+                System.out.println("Type 'finish' to end your work day: ");
+                userStr = scanner.nextLine();
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        workingDay.setFinish();
+        System.out.println("Finished working at: " + workingDay.getFinish());
+        workingDay.calculateDuration();
+        System.out.println(workingDay.getDuration());
     }
 }
--- src/WorkingDay.java
+++ src/WorkingDay.java
@@ -1,2 +1,32 @@
+import java.time.Duration;
+import java.time.OffsetDateTime;
+
 public class WorkingDay {
+    OffsetDateTime start;
+    OffsetDateTime finish;
+    Duration duration;
+
+    public OffsetDateTime getStart() {
+        return start;
+    }
+
+    public void setStart() {
+        this.start = OffsetDateTime.now();
+    }
+
+    public OffsetDateTime getFinish() {
+        return finish;
+    }
+
+    public void setFinish() {
+        this.finish = OffsetDateTime.now();
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void calculateDuration() {
+        this.duration = Duration.between(start, finish);
+    }
 }
